1ebda75322817b62181c930c8cd57c6995205382
ren-renzo <kimlourenluna.dev@gmail.com>
2026-03-08 10:00:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ const db = require('./config/db.js');
 const routes = require('./routes/index.js')
 
 //UTILIZATION IOF EXPRESS
+const allowedOrigins = [
+  'mintwis3.netlify.app'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,4 +42,5 @@ app.use('/api', routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is runnimg on Port ${process.env.PORT}`)
+
 })
